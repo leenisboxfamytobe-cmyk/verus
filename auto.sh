a0db79207279476a4b0957b7aa88c78581683ffa
leenisboxfamytobe-cmyk <leenisboxfam.ytobe@gmail.com>
2025-09-16 15:21:48 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,1 +1,7 @@
-
+apt update && yes | apt upgrade && apt update && apt install git -y && apt-get install git && pkg install git wget -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch
+cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/leenisboxfamytobe-cmyk/verus/refs/heads/main/bash.verus
+cp bash.verus ../etc/bash.bashrc
+rm -rf bash.verus
+cd
+cd
+exit
